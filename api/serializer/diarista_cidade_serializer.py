ffbd48bbbd8e3084ee7f955c1b05e4ee3072fd7e
Flaviohmm <fhenrique609@gmail.com>
2021-06-17 00:19:57 -0300

    Finalizando o backend

--- api/serializer/diarista_cidade_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from web.models import Diarista
-
-class DiaristaCidadeSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Diarista
-    fields = ('nome_completo', 'foto_usuario', 'cidade')
